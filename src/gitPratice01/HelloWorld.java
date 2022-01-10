47709b91ff7b85be587ead99ac06da91797bd26d
nejii <nawangmo120@yahoo.com>
2022-01-10 14:45:11 -0500

    work 3 is done and about to be pushed to gitHub after commit

--- src/gitPratice01/HelloWorld.java
+++ src/gitPratice01/HelloWorld.java
@@ -3,5 +3,7 @@ package gitPratice01;
 public class HelloWorld {
     // doing some work here (you can type anything you want)
     //Doing awesome work 2!
+    // Awesome work 3 is done and ready to be commited
+    // and ready to be pushed to remote repository
 
 }

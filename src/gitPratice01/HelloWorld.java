046f406c74662fb7645de35ec983f321407ab1fa
Nagwang Wangmo <nawangmo120@yahoo.com>
2022-01-11 14:30:42 -0500

    added a folder called power into the list

--- src/gitPratice01/HelloWorld.java
+++ src/gitPratice01/HelloWorld.java
@@ -3,7 +3,11 @@ package gitPratice01;
 public class HelloWorld {
     // doing some work here (you can type anything you want)
     //Doing awesome work 2!
-    // Awesome work 3 is done and ready to be commited
+    // Awesome work 3 is done and ready to be commit
     // and ready to be pushed to remote repository
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
 }
